a7d40ff3159a34cae9e9e8f2bb99429e4b49f379
Ashishsavarn <160284243+Ashishsavarn@users.noreply.github.com>
2024-09-25 12:30:48 +0530

    Create Infix Expression to Postfix Expression.c

---
+++ Infix Expression to Postfix Expression.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX 100
+
+typedef struct {
+    int top;
+    char items[MAX];
+} Stack;
+
+void initStack(Stack *s);
+int isFull(Stack *s);
+int isEmpty(Stack *s);
+void push(Stack *s, char item);
+char pop(Stack *s);
+char peek(Stack *s);
+int precedence(char op);
+int isOperator(char c);
+void infixToPostfix(const char *infix, char *postfix);
+
+int main() {
+    char infix[MAX], postfix[MAX];
+
+    printf("Enter an infix expression: ");
+    fgets(infix, sizeof(infix), stdin);
+    infix[strcspn(infix, "\n")] = 0;
+
+    infixToPostfix(infix, postfix);
+    printf("Postfix expression: %s\n", postfix);
+
+    return 0;
+}
+void initStack(Stack *s) {
+    s->top = -1;
+}
+int isFull(Stack *s) {
+    return s->top == MAX - 1;
+}
+int isEmpty(Stack *s) {
+    return s->top == -1;
+}
+void push(Stack *s, char item) {
+    if (!isFull(s)) {
+        s->items[++s->top] = item;
+    }
+}
+char pop(Stack *s) {
+    if (!isEmpty(s)) {
+        return s->items[s->top--];
+    }
+    return '\0'; 
+}
+char peek(Stack *s) {
+    if (!isEmpty(s)) {
+        return s->items[s->top];
+    }
+    return '\0'; 
+}
+int precedence(char op) {
+    switch (op) {
+        case '+':
+        case '-':
+            return 1;
+        case '*':
+        case '/':
+        case '%':
+            return 2;
+        case '^':
+            return 3;
+        default:
+            return 0; 
+    }
+}
+int isOperator(char c) {
+    return (c == '+' || c == '-' || c == '*' || c == '/' || c == '%' || c == '^');
+}
+void infixToPostfix(const char *infix, char *postfix) {
+    Stack s;
+    initStack(&s);
+    int j = 0;
+
+    for (int i = 0; infix[i] != '\0'; i++) {
+        char current = infix[i];
+
+        if (isalnum(current))
+            postfix[j++] = current;
+        } else if (current == '(') { 
+            push(&s, current);
+        } else if (current==')') {
+            while (!isEmpty(&s) && peek(&s)!='(') {
+                postfix[j++]=pop(&s);
+            }
+            pop(&s);
+        } else if (isOperator(current)) {
+            while (!isEmpty(&s) && precedence(peek(&s))>=precedence(current)) {
+                postfix[j++]=pop(&s);
+            }
+            push(&s, current); 
+        }
+    }
+    while (!isEmpty(&s)) {
+        postfix[j++] = pop(&s);
+    }
+
+    postfix[j] = '\0';  
+}
